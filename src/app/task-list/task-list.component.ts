96ee37e8d3a2d8d9df2eeeea5515ae7a8a82bbc6
Nicolas-Ortiz <nicortiz1234@gmail.com>
2024-03-29 20:02:42 -0300

    add functionality to create task and mark task as complete

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -11,7 +11,26 @@ import { DataInputBoxComponent } from '../data-input-box/data-input-box.componen
 })
 export class TaskListComponent {
   receivedTask!: Task;
+
   taskList: Task[] = [];
+  allTaskList: Task[] = [];
+  completedTaskList: Task[] = [];
+  activeTaskList: Task[] = [];
+
+  /* Variables to show tasks */
+  all: boolean = true;
+  active: boolean = false;
+  completed: boolean = false;
+
+  showTasks() {
+    if (this.all) {
+      this.taskList = this.allTaskList;
+    } else if (this.active) {
+      this.taskList = this.activeTaskList;
+    } else {
+      this.taskList = this.completedTaskList;
+    }
+  }
 
   receiveTask(task: Task) {
     this.receivedTask = task;
@@ -19,6 +38,51 @@ export class TaskListComponent {
   }
 
   addTask(task: Task) {
-    this.taskList.push(task);
+    this.allTaskList.push(task);
+    this.activeTaskList.push(task);
+    this.showTasks();
   }
+
+  allBtnHandle() {
+    this.all = true;
+    this.active = false;
+    this.completed = false;
+    this.showTasks();
+  }
+
+  activeBtnHandle() {
+    this.all = false;
+    this.active = true;
+    this.completed = false;
+    this.showTasks();
+  }
+
+  completedBtnHandle() {
+    this.all = false;
+    this.active = false;
+    this.completed = true;
+    this.showTasks();
+  }
+
+  clearCompletedTasks() {
+    this.taskList = this.taskList.filter((elem) => {
+      elem.status = true;
+    });
+    this.completedTaskList = [];
+  }
+
+  addCompletedTask(task: Task) {
+    if (task.status === true) {
+      task.status = false;
+      this.completedTaskList.push(task);
+      this.deleteTask(this.activeTaskList, task);
+    }
+  }
+
+  deleteTask(list: Task[], task: Task) {
+    const index : number = list.indexOf(task);
+    if (index !== -1) {
+      list.splice(index, 1);
+    }
+  } 
 }
