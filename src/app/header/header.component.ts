3d17a2da1f526a9820a090864c6d8ee77451054f
Nicolas-Ortiz <nicortiz1234@gmail.com>
2024-04-02 16:07:26 -0300

    Create button to change color theme

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { DataInputBoxComponent } from '../data-input-box/data-input-box.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [DataInputBoxComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  lightTheme: boolean = true;
+  
+  constructor(private dataInputBoxCmp: DataInputBoxComponent) {
+  }
+
+  buttonThemeHandle() {
+    if (this.lightTheme) {
+      /* Active dark theme */
+      document.querySelector('.header')?.classList.toggle('dark');
+      this.dataInputBoxCmp.changeDataInputBoxTheme();
+      
+    } else {
+      document.querySelector('.header')?.classList.toggle('dark');
+      this.dataInputBoxCmp.changeDataInputBoxTheme();
+    }
+    this.lightTheme = !this.lightTheme;
+  }
 
 }
